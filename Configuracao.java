0a0615405ddac3964aff2679ae5082497c29605b
Pedro Xavier <pedro.wendelstein@edu.pucrs.br>
2022-11-05 09:24:09 -0300

    refatorando codigo

---
+++ Configuracao.java
@@ -0,0 +1,21 @@
+import java.net.InetAddress;
+
+public class Configuracao {
+    public int id;
+	public InetAddress nodeIp;
+	public int port;
+	public float chance;
+	public int events;
+	public int min_delay;
+	public int max_delay;
+
+    public Configuracao(int id, InetAddress nodeIp, int port, float chance, int events, int min_delay, int max_delay){
+        this.id=id;
+        this.nodeIp=nodeIp;
+        this.port=port;
+        this.chance=chance;
+        this.events=events;
+        this.min_delay=min_delay;
+        this.max_delay=max_delay;
+    }
+}
--- Main.java
+++ Main.java
@@ -1,71 +1,45 @@
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
+import java.net.UnknownHostException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
+	private static Configuracao configuracao;
+	private static int qtdDeProc;
+	private static List processos;
+	private static List<Integer> ports;
 
 	public static void main(String[] args) throws IOException {
-		// configurando
-		File confFile = new File("config.txt");
-		Scanner reader = new Scanner(confFile);
-		int linha = Integer.parseInt(args[0]);
-		LinkedList<InetAddress> hosts = new LinkedList<>();
-		List<Integer> ports = new LinkedList<>();
-		int i = 0;
-		while(i < linha) {
-			String[] campos = reader.nextLine().split(" ");
-			hosts.add(InetAddress.getByName(campos[1]));
-			ports.add(Integer.parseInt(campos[2]));
-			i++;
-		}
-		int id = reader.nextInt();
-		InetAddress nodeIp = InetAddress.getByName(reader.next());
-		int port = reader.nextInt();
-		float chance = reader.nextFloat();
-		int events = reader.nextInt();
-		int min_delay = reader.nextInt();
-		int max_delay = reader.nextInt();
-		if(reader.hasNextLine()){
-			reader.nextLine();
-			i++;
-		}
-
-		// quantidade de processos no arquivo de configuracao
-		while(reader.hasNextLine()){
-			String[] campos = reader.nextLine().split(" ");
-			hosts.add(InetAddress.getByName(campos[1]));
-			ports.add(Integer.parseInt(campos[2]));
-			i++;
-		}
-		int qtdDeProc = i;
-		List processos = new LinkedList<Integer>();
+		// Le arquivo de configuracao e usa a configuracao com o numero da linha passada por terminal no primeiro argumento
+		LeituraArquivo(args[0]);
 
 		// criando o multicast
-		byte[] buffer=new byte[1024];
+		byte[] buffer = new byte[1024];
 		MulticastSocket socket = new MulticastSocket(4321);
 		InetAddress grupo = InetAddress.getByName("230.0.0.0");
 		socket.joinGroup(grupo);
 
 		// vai ficar enviando ate todos chegarem
-		SendArrived sendArrived = new SendArrived(port);
+		SendArrived sendArrived = new SendArrived(configuracao.port);
 		sendArrived.start();
 
 		// vai ficar escutando ate todos chegarem
-		while(true) {
-			if( processos.size() == qtdDeProc) {
+		while (true) {
+			if (processos.size() == qtdDeProc) {
 				break;
 			}
 			System.out.println("Esperando pelos outros...");
 			DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
 			socket.receive(packet);
 			String msg = new String(packet.getData(),
-			packet.getOffset(),packet.getLength());
-			if( !processos.contains(Integer.parseInt(msg)) ) {
+					packet.getOffset(), packet.getLength());
+			if (!processos.contains(Integer.parseInt(msg))) {
 				processos.add(Integer.parseInt(msg));
 			}
 			System.out.println(msg + " chegou.");
@@ -74,17 +48,51 @@ public class Main {
 		sendArrived.stopThread();
 		System.out.println("Parou!");
 		ports.stream()
-			.forEach(System.out::println);
+				.forEach(System.out::println);
 
 		// eventos
-
 
 		// int ttl = socket.getTimeToLive();
 		// socket.setTimeToLive(20);
 
 		// byte[] bytePort = Integer.toString(port).getBytes();
-		// DatagramPacket packet = new DatagramPacket(bytePort, bytePort.length, grupo, 9000);
+		// DatagramPacket packet = new DatagramPacket(bytePort, bytePort.length, grupo,
+		// 9000);
 		// socket.send(packet);
 		// socket.setTimeToLive(ttl);
+	}
+
+	public static void LeituraArquivo(String id) throws IOException {
+		// configurando
+		File confFile = new File("config.txt");
+		Scanner reader = new Scanner(confFile);
+		int linha = Integer.parseInt(id);
+		LinkedList<InetAddress> hosts = new LinkedList<>();
+		ports = new LinkedList<>();
+		int i = 1;
+		while (i < linha) {
+			String[] campos = reader.nextLine().split(" ");
+			hosts.add(InetAddress.getByName(campos[1]));
+			ports.add(Integer.parseInt(campos[2]));
+			i++;
+		}
+		configuracao = new Configuracao(reader.nextInt(),
+				InetAddress.getByName(reader.next()), reader.nextInt(),
+				reader.nextFloat(), reader.nextInt(), reader.nextInt(),
+				reader.nextInt());
+		if (reader.hasNextLine()) {
+			reader.nextLine();
+			i++;
+		}
+
+		// quantidade de processos no arquivo de configuracao
+		while (reader.hasNextLine()) {
+			String[] campos = reader.nextLine().split(" ");
+			hosts.add(InetAddress.getByName(campos[1]));
+			ports.add(Integer.parseInt(campos[2]));
+			i++;
+		}
+		qtdDeProc = i;
+		processos = new LinkedList<Integer>();
 	}
 }
